aae6916a2ef19b95126e5505e5e2c9f72077b5d4
Charles Ménier <char.menier@gmail.com>
2019-12-17 08:54:52 +0100

    Day10 part 1

--- Day10.js
+++ Day10.js
@@ -100,79 +100,123 @@ const parse = data => data
 const getPGCD = (a, b) => (a ? getPGCD(b % a, a) : b);
 
 
-const findNumberOfVisibleAsteroid = (asteroids, x, y) => {
+const findVisibleAsteroids = (asteroids, x, y) => {
 
     //console.log('\n *** Checking for asteroid at x=', x, 'y=', y, ' *** ');
 
     // Deep clone array
     const visibleAsteroids = JSON.parse(JSON.stringify(asteroids));
 
-    //console.log(visibleAsteroids);
-
     // Asteroid is visible from another one only if it's the first between the two of them in the trajectory
-    for(let j = 0; j < asteroids.length; j++) {
-        for(let i = 0; i < asteroids[j].length; i++) {
+    visibleAsteroids.forEach((line, j) => {
+        line.forEach((isAsteroid, i) => {
 
-            if(x !== i && y !== j) {
+            if((x !== i || y !== j) && isAsteroid) {
 
-                if (asteroids[j][i]) {
+                //console.log('\nCheck with ', {i, j});
 
-                    //console.log('\nCheck between ', {i, j});
+                const pgcd = Math.abs(getPGCD(i - x, j - y));
 
-                    const pgcd = Math.abs(getPGCD(i - x, j - y));
+                let coords = { x: i, y: j };
+                const delta = { x: (i - x) / pgcd, y: (j - y) / pgcd };
+                let notVisibleCoords = { x: i + delta.x, y: j + delta.y };
 
-                    let coords = {x: i, y: j};
-                    const delta = {x: (i - x) / pgcd, y: (j - y) / pgcd};
+                //printCurrentMap(visibleAsteroids, {x, y}, coords);
 
-                    while (coords.x !== x && coords.y !== y) {
-                        //console.log('Found new coords between them', coords);
+                // All asteroids after this one in the same line are not visible
+                while(
+                    (notVisibleCoords.x >= 0 && notVisibleCoords.x <= line.length - 1) &&
+                    (notVisibleCoords.y >= 0 && notVisibleCoords.y <= visibleAsteroids.length - 1)
+                ) {
+                    if(visibleAsteroids[notVisibleCoords.y][notVisibleCoords.x]) {
+                        //console.log('Found new not visible asteroid at', notVisibleCoords);
+                        //printCurrentMap(visibleAsteroids, {x, y}, notVisibleCoords);
+                        visibleAsteroids[notVisibleCoords.y][notVisibleCoords.x] = false;
+                    }
+                    notVisibleCoords = {x: notVisibleCoords.x + delta.x, y: notVisibleCoords.y + delta.y};
+                }
 
-                        if (visibleAsteroids[coords.y - delta.y][coords.x - delta.x]) {
-                            visibleAsteroids[coords.y - delta.y][coords.x - delta.x] = false;
-                        }
+                while (coords.x !== x && coords.y !== y) {
+                    // Coords for next asteroid between current and origin
+                    const newCoords = { x: coords.x - delta.x, y: coords.y - delta.y };
 
-                        coords = {x: coords.x - delta.x, y: coords.y - delta.y};
+                    // There is an asteroid at new position and it's not the one we're on
+                    if (
+                        visibleAsteroids[newCoords.y][newCoords.x] &&
+                        newCoords.x !== x && newCoords.y !== y
+                    ) {
+                        visibleAsteroids[coords.y][coords.x] = false;
+                        //console.log('Found new asteroid at', newCoords);
+                        //printCurrentMap(visibleAsteroids, {x, y}, newCoords);
                     }
 
+                    coords = {x: coords.x - delta.x, y: coords.y - delta.y};
                 }
             }
-        }
-    }
+        });
+    });
 
-    //console.log('Found', visibleAsteroids.reduce((acc, val) => acc.concat(val), []).filter(a => a).length + 1);
+    //console.log('Found', visibleAsteroids.reduce((acc, val) => acc.concat(val), []).filter(a => a).length);
 
-    return visibleAsteroids.reduce((acc, val) => acc.concat(val), []).filter(a => a).length + 1;
+    return visibleAsteroids.reduce((acc, val) => acc.concat(val), []).filter(a => a).length - 1;
 
 };
 
 
-const findMaxDetectedAsteroids = asteroids => {
+const findMaxVisibleAsteroids = asteroids => {
 
     let max = 0;
 
-    for(let y = 0; y < asteroids.length; y++) {
-        for(let x = 0; x < asteroids[y].length; x++) {
-
-            if(asteroids[y][x]) {
-
-                const visible = findNumberOfVisibleAsteroid(asteroids.slice(), x, y);
+    asteroids.forEach((line, y) => {
+        line.forEach((isAsteroid, x) => {
+            if(isAsteroid) {
+                const visible = findVisibleAsteroids(asteroids, x, y);
                 if(visible > max) {
                     max = visible;
                 }
             }
-        }
-    }
+        });
+    });
 
     return max;
 };
 
+
+const printCurrentMap = (asteroids, current, coords) => {
+
+    asteroids.forEach((line, y) => {
+        let visual = '';
+        line.forEach((a, x) => {
+
+            // Current asteroid we're checking the visible for
+            if (current.x === x && current.y === y) {
+                visual += '(O)';
+            } else if (!a) {
+                // No asteroid at position
+                visual += ' . ';
+            } else {
+                if (coords.x === x && coords.y === y) {
+                    // The current position we're checking
+                    visual += '(X)';
+                } else {
+                    // Standard asteroid
+                    visual += ' # ';
+                }
+            }
+        });
+        console.log(visual.normalize());
+    })
+
+};
+
 const testPart1 = () => {
 
-    console.log('Should output 33 : ', findMaxDetectedAsteroids(parse(t1)));
-    console.log('Should output 35 : ', findMaxDetectedAsteroids(parse(t2)));
-    //console.log('Should output 41 : ', findMaxDetectedAsteroids(parse(t3)));
-    //console.log('Should output 210 : ', findMaxDetectedAsteroids(parse(t4)));
+    //console.log('Should output 33 : ', findMaxVisibleAsteroids(parse(t1)));
+    //console.log('Should output 35 : ', findMaxVisibleAsteroids(parse(t2)));
+    //console.log('Should output 41 : ', findMaxVisibleAsteroids(parse(t3)));
+    //console.log('Should output 210 : ', findMaxVisibleAsteroids(parse(t4)));
 
+    console.log('Part 1:', findMaxVisibleAsteroids(parse(input)));
 };
 
 testPart1();
